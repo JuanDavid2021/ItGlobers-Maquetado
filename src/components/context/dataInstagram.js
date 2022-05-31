90fca51e9b72a58085d2c8d75fbee961f56da8c8
JuanDavid2021 <davidyk_15@hotmail.com>
2022-05-30 21:46:32 -0500

    add env

--- src/components/context/dataInstagram.js
+++ src/components/context/dataInstagram.js
@@ -12,7 +12,7 @@ const InstagramProvider =(props)=>{
     useEffect( ()=>{
   
         const peticion = async()=>{
-            const response = await axios.get(`https://graph.instagram.com/me/media?fields=id,media_url,thumbnail_url,permalink&access_token=IGQVJXbHhBN0JUX3NkWnFYSlZAhbEF2Q2pFTlB4MjhDR0NoQzQ4anZAMZAlpqV0Y3ZA1NKdDROdlloUHp5N09pNzEtVDBsMnZAQOGxKUFRWTGplNnNLeXhoQ2VIYmEzN0JqYjNzdGo0M2x2NXJuRENUWDhJMgZDZD`) 
+            const response = await axios.get(`https://graph.instagram.com/me/media?fields=id,media_url,thumbnail_url,permalink&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`) 
             setPhotos(response.data)
         } 
         peticion()
